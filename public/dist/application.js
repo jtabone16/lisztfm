07908cf6e52a6c0c6b6fd7bbe57db23e3aee1547
John Tabone <jtabone16@gmail.com>
2015-08-11 16:13:00 -0400

    Fixed horizontal overflow bug found in twitter bootstrap 3.x for container-fluid

--- public/dist/application.js
+++ public/dist/application.js
@@ -368,31 +368,34 @@ angular.module('playlists').controller('PlaylistsController', ['$scope', '$http'
 					}
 				}).
 				error(function(err){
+					$http.get('/auth/refresh');
 					//TODO: more efficient refreshing
 				});
 		};
 
-		$http.get('/auth/refresh').
-			success(function(resp){
-				$window.user.providerData.accessToken = resp.token;
-				$scope.playlist_req = {
-					 method: 'GET' ,
-					 url: 'https://api.spotify.com/v1/users/' + $window.user.username + '/playlists',
-					 headers: {
-						 'Authorization': 'Bearer ' + $window.user.providerData.accessToken
-					 },
-				};
+		// $http.get('/auth/refresh').
+		// 	success(function(resp){
+		// 		$window.user.providerData.accessToken = resp.token;
+		// 		$scope.playlist_req = {
+		// 			 method: 'GET' ,
+		// 			 url: 'https://api.spotify.com/v1/users/' + $window.user.username + '/playlists',
+		// 			 headers: {
+		// 				 'Authorization': 'Bearer ' + $window.user.providerData.accessToken
+		// 			 },
+		// 		};
+		//
+		// 		$scope.track_req = {
+		// 			 method: 'GET' ,
+		// 			 url: 'https://api.spotify.com/v1/users/' + $window.user.username + '/playlists/' +  $scope.currentPlaylist.id + '/tracks',
+		// 			 headers: {
+		// 				 'Authorization': 'Bearer ' + $window.user.providerData.accessToken
+		// 			 },
+		// 		};
+		//
+		// 		$scope.getPlaylists();
+		// 	});
 
-				$scope.track_req = {
-					 method: 'GET' ,
-					 url: 'https://api.spotify.com/v1/users/' + $window.user.username + '/playlists/' +  $scope.currentPlaylist.id + '/tracks',
-					 headers: {
-						 'Authorization': 'Bearer ' + $window.user.providerData.accessToken
-					 },
-				};
-
-				$scope.getPlaylists();
-			});
+		$scope.getPlaylists();
 
 
 		$scope.getTracks = function(req){
@@ -438,7 +441,7 @@ angular.module('playlists').controller('PlaylistsController', ['$scope', '$http'
 					}
 				}).
 				error(function(err){
-
+					$http.get('/auth/refresh');
 				});
 		};
 
@@ -556,7 +559,7 @@ angular.module('playlists').controller('PlaylistsController', ['$scope', '$http'
 						});
 				}).
 				error(function (res){
-
+					$http.get('/auth/refresh');
 				});
 
 		};
